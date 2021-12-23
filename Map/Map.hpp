19895377a8b4ce9b6f4c3966dee6568efcc1ad2b
sandul <sandulvp@sibur.ru>
2021-12-23 13:52:20 +0300

    Map dir added

---
+++ Map/Map.hpp
@@ -0,0 +1,29 @@
+#ifndef Map_hpp
+# define Map_hpp
+
+# include <memory> // std::allocator<T>
+
+namespace ft {
+
+template<class T, class Allocator = std::allocator<T> >
+class Map
+{
+    public:
+
+        /* * * * * * * * * * * * * * * * * * * */
+
+        /* * * * * * Element access: * * * * * */
+
+        /* * * * * * Capacity: * * * * * * * * */
+
+        /* * * * * * Modifiers:  * * * * * * * */
+
+    private:
+
+};
+
+/* * * * * * Non-member functions:  * * * * * */
+
+}  // namespace ft
+
+#endif
---
+++ Map/Map_test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <map>
+
+#include <memory> // std::allocator<T>
+
+#include "Map.hpp"
+
+int main(void)
+{
+    std::cout << "Start!\n";
+    std::cout << '\n';
+
+    // ...
+
+    std::cout << '\n';
+    std::cout << "Finish\n";
+    return (0);
+}
