2681b95d5c10bd15445e4497451e600e0a680b29
sandul <sandulvp@sibur.ru>
2021-12-23 13:51:39 +0300

    Vector dir added

---
+++ Vector/Vector_test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+
+#include <memory> // std::allocator<T>
+
+#include "Vector.hpp"
+
+int main(void)
+{
+    std::cout << "Start!\n";
+    std::cout << '\n';
+
+    // ...
+
+    std::cout << '\n';
+    std::cout << "Finish\n";
+    return (0);
+}
